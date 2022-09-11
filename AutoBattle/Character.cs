f5557ec3e201411fac5f5c4b9ace06aca19a93b2
Juan <juanpaixao@live.com>
2022-09-11 14:31:33 -0300

    Start Turn Fix (description)
    Some of the conditions are wrong, return was on wrong place and to check up or down conditions the code was using X instead of Y, so it was not working properly.
    
    Also fix the description to detect check close targets.

--- AutoBattle/Character.cs
+++ AutoBattle/Character.cs
@@ -14,20 +14,21 @@ namespace AutoBattle
         public float DamageMultiplier { get; set; }
         public GridBox currentBox;
         public int PlayerIndex;
-        public Character Target { get; set; } 
+        public Character Target { get; set; }
+
         public Character(CharacterClass characterClass)
         {
-
         }
 
 
         public bool TakeDamage(float amount)
         {
-            if((Health -= BaseDamage) <= 0)
+            if ((Health -= BaseDamage) <= 0)
             {
                 Die();
                 return true;
             }
+
             return false;
         }
 
@@ -38,22 +39,21 @@ namespace AutoBattle
 
         public void WalkTO(bool CanWalk)
         {
-
         }
 
         public void StartTurn(Grid battlefield)
         {
-
-            if (CheckCloseTargets(battlefield)) 
+            if (CheckCloseTargets(battlefield))
             {
                 Attack(Target);
-                
+
 
                 return;
             }
             else
-            {   // if there is no target close enough, calculates in wich direction this character should move to be closer to a possible target
-                if(this.currentBox.xIndex > Target.currentBox.xIndex)
+            {
+                // if there is no target close enough, calculates in wich direction this character should move to be closer to a possible target
+                if (this.currentBox.xIndex > Target.currentBox.xIndex)
                 {
                     if ((battlefield.grids.Exists(x => x.Index == currentBox.Index - 1)))
                     {
@@ -61,41 +61,47 @@ namespace AutoBattle
                         battlefield.grids[currentBox.Index] = currentBox;
                         currentBox = (battlefield.grids.Find(x => x.Index == currentBox.Index - 1));
                         currentBox.ocupied = true;
+
                         battlefield.grids[currentBox.Index] = currentBox;
                         Console.WriteLine($"Player {PlayerIndex} walked left\n");
                         battlefield.drawBattlefield(5, 5);
 
                         return;
                     }
-                } else if(currentBox.xIndex < Target.currentBox.xIndex)
+                }
+                else if (currentBox.xIndex < Target.currentBox.xIndex)
                 {
                     currentBox.ocupied = false;
                     battlefield.grids[currentBox.Index] = currentBox;
                     currentBox = (battlefield.grids.Find(x => x.Index == currentBox.Index + 1));
                     currentBox.ocupied = true;
-                    return;
+
                     battlefield.grids[currentBox.Index] = currentBox;
                     Console.WriteLine($"Player {PlayerIndex} walked right\n");
                     battlefield.drawBattlefield(5, 5);
+
+                    return;
                 }
 
                 if (this.currentBox.yIndex > Target.currentBox.yIndex)
                 {
-                    battlefield.drawBattlefield(5, 5);
                     this.currentBox.ocupied = false;
                     battlefield.grids[currentBox.Index] = currentBox;
-                    this.currentBox = (battlefield.grids.Find(x => x.Index == currentBox.Index - battlefield.xLenght));
+                    this.currentBox = (battlefield.grids.Find(x => x.Index == currentBox.Index - battlefield.yLength));
                     this.currentBox.ocupied = true;
+
                     battlefield.grids[currentBox.Index] = currentBox;
                     Console.WriteLine($"Player {PlayerIndex} walked up\n");
+                    battlefield.drawBattlefield(5, 5);
                     return;
                 }
-                else if(this.currentBox.yIndex < Target.currentBox.yIndex)
+                else if (this.currentBox.yIndex < Target.currentBox.yIndex)
                 {
-                    this.currentBox.ocupied = true;
-                    battlefield.grids[currentBox.Index] = this.currentBox;
-                    this.currentBox = (battlefield.grids.Find(x => x.Index == currentBox.Index + battlefield.xLenght));
                     this.currentBox.ocupied = false;
+                    battlefield.grids[currentBox.Index] = this.currentBox;
+                    this.currentBox = (battlefield.grids.Find(x => x.Index == currentBox.Index + battlefield.yLength));
+                    this.currentBox.ocupied = true;
+
                     battlefield.grids[currentBox.Index] = currentBox;
                     Console.WriteLine($"Player {PlayerIndex} walked down\n");
                     battlefield.drawBattlefield(5, 5);
@@ -110,21 +116,23 @@ namespace AutoBattle
         {
             bool left = (battlefield.grids.Find(x => x.Index == currentBox.Index - 1).ocupied);
             bool right = (battlefield.grids.Find(x => x.Index == currentBox.Index + 1).ocupied);
-            bool up = (battlefield.grids.Find(x => x.Index == currentBox.Index + battlefield.xLenght).ocupied);
-            bool down = (battlefield.grids.Find(x => x.Index == currentBox.Index - battlefield.xLenght).ocupied);
+            bool up = (battlefield.grids.Find(x => x.Index == currentBox.Index + battlefield.yLength).ocupied);
+            bool down = (battlefield.grids.Find(x => x.Index == currentBox.Index - battlefield.yLength).ocupied);
 
-            if (left & right & up & down) 
+            if (left || right || up || down)
             {
                 return true;
             }
-            return false; 
+
+            return false;
         }
 
-        public void Attack (Character target)
+        public void Attack(Character target)
         {
             var rand = new Random();
             target.TakeDamage(rand.Next(0, (int)BaseDamage));
-            Console.WriteLine($"Player {PlayerIndex} is attacking the player {Target.PlayerIndex} and did {BaseDamage} damage\n");
+            Console.WriteLine(
+                $"Player {PlayerIndex} is attacking the player {Target.PlayerIndex} and did {BaseDamage} damage\n");
         }
     }
 }
